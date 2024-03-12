a56c3128d9e8a40b62d711618742f2dbb6aacc4d
Pedro Hurtado <pedrohurt@gmail.com>
2024-03-12 09:59:33 +0100

    router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { Pagina1Component } from './pages/pagina1/pagina1.component';
+/*import { Pagina1Component } from './pages/pagina1/pagina1.component';
 import { Pagina2Component } from './pages/pagina2/pagina2.component';
-import { Pagina3Component } from './pages/pagina3/pagina3.component';
+import { Pagina3Component } from './pages/pagina3/pagina3.component';*/
 
-export const routes: Routes = [
+/*export const routes: Routes = [
   {
     path:"pagina1", component:Pagina1Component
   },
@@ -12,5 +12,24 @@ export const routes: Routes = [
   },
   {
     path:"pagina3", component:Pagina3Component
+  }
+];*/
+
+export const routes: Routes = [
+  {
+    path:"pagina1/:id", loadComponent:
+      ()=>import('./pages/pagina1/pagina1.component')
+        .then(c=>c.Pagina1Component)
+  },
+  {
+    path:"pagina2", loadComponent:
+     ()=>import('./pages/pagina2/pagina2.component')
+     .then(c=>c.Pagina2Component)
+
+  },
+  {
+    path:"pagina3", loadComponent:
+    ()=>import('./pages/pagina3/pagina3.component')
+    .then(c=>c.Pagina3Component)
   }
 ];
